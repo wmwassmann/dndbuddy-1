e866862f0e527e4484b753fa7941b14a95752638
William Wassmann <wmwassmann@hotmail.com>
2020-07-28 19:02:23 -0700

    adding image function

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -2,6 +2,215 @@ $(document).ready(function () {
 	$(document).foundation();
 	$('#Modal1').foundation('open');
 
+
+
+	// Will's stuff!
+
+// arrays with images
+var mhf = ["images-male/human-fighter-1.jpg", "images-male/human-fighter-2.jpg", "images-male/human-fighter-3.jpg"];
+var mhra = ["images-male/human-ranger-1.jpg", "images-male/human-ranger-2.jpg", "images-male/human-ranger-3.jpg"];
+var mhm = ["images-male/human-mage-1.jpg", "images-male/human-mage-2.jpg", "images-male/human-mage-3.jpg"];
+var mhc = ["images-male/human-cleric-1.jpg", "images-male/human-cleric-2.jpg", "images-male/human-cleric-3.jpg"];
+var mhro = ["images-male/human-rogue-1.jpg", "images-male/human-rogue-2.jpg", "images-male/human-rogue-3.jpg"];
+var fhf = ["images-female/human-fighter-1.jpg", "images-female/human-fighter-2.jpg", "images-female/human-fighter-3.jpg"];
+var fhra = ["images-female/human-ranger-1.jpg", "images-female/human-ranger-2.jpg", "images-female/human-ranger-3.jpg"];
+var fhm = ["images-female/human-mage-1.jpg", "images-female/human-mage-2.jpg", "images-female/human-mage-3.jpg"];
+var fhc = ["images-female/human-cleric-1.jpg", "images-female/human-cleric-2.jpg", "images-female/human-cleric-3.jpg"];
+var fhro = ["images-female/human-rogue-1.jpg", "images-female/human-rogue-2.jpg", "images-female/human-rogue-3.jpg"];
+var mef = ["images-male/elf-fighter-1.jpg", "images-male/elf-fighter-2.jpg", "images-male/elf-fighter-3.jpg"];
+var fef = ["images-female/elf-fighter-1.jpg", "images-female/elf-fighter-2.jpg", "images-female/elf-fighter-3.jpg"];
+var mera = ["images-male/elf-ranger-1.jpg", "images-male/elf-ranger-2.jpg", "images-male/elf-ranger-3.jpg"];
+var fera = ["images-female/elf-ranger-1.jpg", "images-female/elf-ranger-2.jpg", "images-female/elf-ranger-3.jpg"];
+var mem = ["images-male/elf-mage-1.jpg", "images-male/elf-mage-2.jpg", "images-male/elf-mage-3.jpg"];
+var fem = ["images-female/elf-mage-1.jpg", "images-female/elf-mage-2.jpg", "images-female/elf-mage-3.jpg"];
+var mec = ["images-male/elf-cleric-1.jpg", "images-male/elf-cleric-2.jpg", "images-male/elf-cleric-3.jpg"];
+var fec = ["images-female/elf-cleric-1.jpg", "images-female/elf-cleric-2.jpg", "images-female/elf-cleric-3.jpg"];
+var mero = ["images-male/elf-rogue-1.jpg", "images-male/elf-rogue-2.jpg", "images-male/elf-rogue-3.jpg"];
+var fero = ["images-female/elf-rogue-1.jpg", "images-female/elf-rogue-2.jpg", "images-female/elf-rogue-3.jpg"];
+var mdf = ["images-male/dwarf-fighter-1.jpg", "images-male/dwarf-fighter-2.jpg", "images-male/dwarf-fighter-3.jpg"];
+var fdf = ["images-female/dwarf-fighter-1.jpg", "images-female/dwarf-fighter-2.jpg", "images-female/dwarf-fighter-3.jpg"];
+var mdra = ["images-male/dwarf-ranger-1.jpg", "images-male/dwarf-ranger-2.jpg", "images-male/dwarf-ranger-3.jpg"];
+var fdra = ["images-female/dwarf-ranger-1.jpg", "images-female/dwarf-ranger-2.jpg", "images-female/dwarf-ranger-3.jpg"];
+var mdm = ["images-male/dwarf-mage-1.jpg", "images-male/dwarf-mage-2.jpg", "images-male/dwarf-mage-3.jpg"];
+var fdm = ["images-female/dwarf-mage-1.jpg", "images-female/dwarf-mage-2.jpg", "images-female/dwarf-mage-3.jpg"];
+var mdc = ["images-male/dwarf-cleric-1.jpg", "images-male/dwarf-cleric-2.jpg", "images-male/dwarf-cleric-3.jpg"];
+var fdc = ["images-female/dwarf-cleric-1.jpg", "images-female/dwarf-cleric-2.jpg", "images-female/dwarf-cleric-3.jpg"];
+var mdro = ["images-male/dwarf-rogue-1.jpg", "images-male/dwarf-rogue-2.jpg", "images-male/dwarf-rogue-3.jpg"];
+var fdro = ["images-female/dwarf-rogue-1.jpg", "images-female/dwarf-rogue-2.jpg", "images-female/dwarf-rogue-3.jpg"];
+
+// sets the image equal to the ID in the html
+var result = document.getElementById("centerVidImg");
+
+// searches a random number and is called in each if then statement
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+// generates a random character image based on gender, race, class.
+function randomImage() {
+
+	// plugs in to the local storage
+	var gender = localStorage.getItem("charGender");
+	var race = localStorage.getItem("charRace");
+	var classDisplay = localStorage.getItem("charClass");
+	// display which plugs into var results
+	var currentpicture;
+
+	// human if then statements "Male"
+	if (gender === "Male" && race === "Human" && classDisplay === "Fighter") {
+		currentpicture = mhf[randomNumber(0,mhf.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Human" && classDisplay === "Ranger") {
+		currentpicture = mhra[randomNumber(0,mhra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Human" && classDisplay === "Wizard") {
+		currentpicture = mhm[randomNumber(0,mhra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Human" && classDisplay === "Cleric") {
+		currentpicture = mhc[randomNumber(0,mhra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Human" && classDisplay === "Rogue") {
+		currentpicture = mhro[randomNumber(0,mhra.length)];
+		result.src = currentpicture;
+
+	// human if then statements "Female"
+	} else if (gender === "Female" && race === "Human" && classDisplay === "Fighter") {
+		currentpicture = fhf[randomNumber(0,fhf.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Human" && classDisplay === "Ranger") {
+		currentpicture = fhra[randomNumber(0,fhra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Human" && classDisplay === "Wizard") {
+		currentpicture = fhm[randomNumber(0,fhm.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Human" && classDisplay === "Cleric") {
+		currentpicture = fhc[randomNumber(0,fhc.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Human" && classDisplay === "Rogue") {
+		currentpicture = fhro[randomNumber(0,fhro.length)];
+		result.src = currentpicture;
+
+	// elf if then statements "Male"
+	} else if (gender === "Male" && race === "Elf" && classDisplay === "Fighter") {
+		currentpicture = mef[randomNumber(0,mef.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Elf" && classDisplay === "Ranger") {
+		currentpicture = mera[randomNumber(0,mera.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Elf" && classDisplay === "Wizard") {
+		currentpicture = mem[randomNumber(0,mera.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Elf" && classDisplay === "Cleric") {
+		currentpicture = mec[randomNumber(0,mera.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Elf" && classDisplay === "Rogue") {
+		currentpicture = mero[randomNumber(0,mera.length)];
+		result.src = currentpicture;
+
+	// elf if then statements "Female"
+	} else if (gender === "Female" && race === "Elf" && classDisplay === "Fighter") {
+		currentpicture = fef[randomNumber(0,fef.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Elf" && classDisplay === "Ranger") {
+		currentpicture = fera[randomNumber(0,fera.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Elf" && classDisplay === "Wizard") {
+		currentpicture = fem[randomNumber(0,fem.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Elf" && classDisplay === "Cleric") {
+		currentpicture = fec[randomNumber(0,fec.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Elf" && classDisplay === "Rogue") {
+		currentpicture = fero[randomNumber(0,fero.length)];
+		result.src = currentpicture;
+
+	// dwarf if then statements "Male"
+	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Fighter") {
+		currentpicture = mdf[randomNumber(0,mdf.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Ranger") {
+		currentpicture = mdra[randomNumber(0,mdra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Wizard") {
+		currentpicture = mdm[randomNumber(0,mdra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Cleric") {
+		currentpicture = mdc[randomNumber(0,mdra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Rogue") {
+		currentpicture = mdro[randomNumber(0,mdra.length)];
+		result.src = currentpicture;
+
+	// dwarf if then statements "Female"
+	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Fighter") {
+		currentpicture = fdf[randomNumber(0,fdf.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Ranger") {
+		currentpicture = fdra[randomNumber(0,fdra.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Wizard") {
+		currentpicture = fdm[randomNumber(0,fdm.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Cleric") {
+		currentpicture = fdc[randomNumber(0,fdc.length)];
+		result.src = currentpicture;
+	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Rogue") {
+		currentpicture = fdro[randomNumber(0,fdro.length)];
+		result.src = currentpicture;
+	}
+
+	//updates the src.URL to equal the image
+	$(".centerBioRight img").attr("src",result.src);
+};
+
+	// generate a random number function
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
+
+	// when the user pick random
+	$('#toRandom').click(function () {
+		event.preventDefault();
+
+		// pick the gender
+		var genderNum = getRandomInt(1);
+		if (genderNum === 0) {
+			playerSelection[0] = 'Male';
+		} else {
+			playerSelection[0] = 'Female';
+		}
+
+		// pick a race
+		var raceNum = getRandomInt(2);
+		if (raceNum === 0) {
+			playerSelection[1] = 'Dwarf';
+		} else if (raceNum === 1) {
+			playerSelection[1] = 'Elf';
+		} else {
+			playerSelection[1] = 'Human';
+		}
+
+		// pick a class
+		var classNum = getRandomInt(4);
+		if (classNum === 0) {
+			playerSelection[4] = 'Cleric';
+		} else if (classNum === 1) {
+			playerSelection[4] = 'Fighter';
+		} else if (classNum === 2) {
+			playerSelection[4] = 'Rogue';
+		} else if (classNum === 3) {
+			playerSelection[4] = 'Ranger';
+		} else if (classNum === 4) {
+			playerSelection[4] = 'Wizard';
+		}
+		console.log(playerSelection);
+		saveToLocal(playerSelection);
+		// calls the image to be generated at this point
+		randomImage();
+		// close modal1
+		$('#Modal1').foundation('close');
+	});
+
 	// 1. gender modal
 	$('#toModal2').click(function () {
 		event.preventDefault();
@@ -321,6 +530,7 @@ $(document).ready(function () {
 			var suggestion = 'Rogue';
 			playerSelection.push(suggestion);
 			console.log(playerSelection[4]);
+			saveToLocal(playerSelection);
 		} else if (
 			playerSelection[2] === 'Far' &&
 			playerSelection[3] === 'Weapons'
@@ -328,6 +538,7 @@ $(document).ready(function () {
 			suggestion = 'Ranger';
 			playerSelection.push(suggestion);
 			console.log(playerSelection[4]);
+			saveToLocal(playerSelection);
 		} else if (
 			playerSelection[2] === 'Far' &&
 			playerSelection[3] === 'Spells'
@@ -335,6 +546,7 @@ $(document).ready(function () {
 			suggestion = 'Wizard';
 			playerSelection.push(suggestion);
 			console.log(playerSelection[4]);
+			saveToLocal(playerSelection);
 		} else if (
 			playerSelection[2] === 'Close' &&
 			playerSelection[3] === 'Weapons'
@@ -342,6 +554,7 @@ $(document).ready(function () {
 			suggestion = 'Fighter';
 			playerSelection.push(suggestion);
 			console.log(playerSelection[4]);
+			saveToLocal(playerSelection);
 		} else if (
 			playerSelection[2] === 'Close' &&
 			playerSelection[3] === 'Spells'
@@ -349,15 +562,8 @@ $(document).ready(function () {
 			suggestion = 'Cleric ';
 			playerSelection.push(suggestion);
 			console.log(playerSelection[4]);
+			saveToLocal(playerSelection);
 		}
-
-		//set Race and Class to Local Storage
-		localStorage.setItem("charGender", playerSelection[0]);
-		localStorage.setItem("charRace", playerSelection[1]);
-		localStorage.setItem("charRange", playerSelection[2]);
-		localStorage.setItem("charStyle", playerSelection[3]);
-		localStorage.setItem("charClass", playerSelection[4]);
-
 		console.log(playerSelection);
 
 		// create a div for the modal6
@@ -413,14 +619,29 @@ $(document).ready(function () {
 		$(modal6b2).html('<span aria-hidden="true">&times;</span>');
 		// append the button to the div
 		$(modal6).append(modal6b2);
-
+		// calls the image to be generated at this point
+		randomImage();
+		
 		// close modal5
 		$('#Modal5').foundation('close');
 		// reload the foundation and open the modal6
 		$(document).foundation();
 		$('#Modal6').foundation('open');
+		
+		
+	
 	});
 
+	//set Race and Class to Local Storage
+	function saveToLocal(playerSelection) {
+		localStorage.setItem('charGender', playerSelection[0]);
+		localStorage.setItem('charRace', playerSelection[1]);
+		localStorage.setItem('charRange', playerSelection[2]);
+		localStorage.setItem('charStyle', playerSelection[3]);
+		localStorage.setItem('charClass', playerSelection[4]);
+	}
+	
+	
 	// 6. video modal
 	$(document).on('click', '.toModal7', function () {
 		event.preventDefault();
